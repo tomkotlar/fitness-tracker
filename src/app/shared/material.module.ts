3f58e426c87570eb93765393ee66b744df005dc9
tomkotlar <t.kotlar@yahoo.com>
2020-04-20 15:26:42 +0100

    adding component structure

---
+++ src/app/shared/material.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from "@angular/core";
+import { MatButtonModule } from "@angular/material/button";
+import { MatIconModule } from "@angular/material/icon";
+
+@NgModule({
+    imports: [
+        MatButtonModule,
+        MatIconModule,
+
+    ],
+    exports: [
+        MatButtonModule,
+        MatIconModule,
+    ]
+})
+
+export class MaterialModule { }
