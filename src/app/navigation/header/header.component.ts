df2d901d41d150356b4f9f5c1445661e30cfb8fd
tomkotlar <t.kotlar@yahoo.com>
2020-04-27 12:47:39 +0100

    updating header / implementing auth user service

--- src/app/navigation/header/header.component.ts
+++ src/app/navigation/header/header.component.ts
@@ -1,18 +1,31 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output, OnDestroy } from '@angular/core';
+import { AuthService } from 'src/app/auth/auth.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
-@Output() toggleSidenav = new EventEmitter<void>()
-  constructor() { }
+export class HeaderComponent implements OnInit, OnDestroy {
+  @Output() toggleSidenav = new EventEmitter<void>()
+  
+  isAuth: boolean = false
+  authSubscription: Subscription
 
-  ngOnInit(): void {
+  constructor( private authService: AuthService ) { }
+
+  ngOnInit() {
+   this.authSubscription = this.authService.authChange.subscribe( authStatus => {
+      this.isAuth = authStatus
+    })
   }
 
   onToggleSideNav() {
 this.toggleSidenav.emit()
+  }
+
+  ngOnDestroy() {
+    this.authSubscription.unsubscribe()
   }
 }
