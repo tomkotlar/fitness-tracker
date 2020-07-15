0defed0707d6e1fbcb457d85e7f8efb7a4eef5b3
tomkotlar <t.kotlar@yahoo.com>
2020-07-15 20:34:51 +0100

    Build -Spinner & SnackBar for wrong cred

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,19 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { AuthService } from '../auth.service';
 import { AngularFireAuth } from "@angular/fire/auth";
+import { UIService } from 'src/app/shared/ui.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent implements OnInit, OnDestroy {
   loginForm: FormGroup
-
-  constructor( private authService: AuthService ) { }
+  isLoading = false
+  private loadingSubs: Subscription
+  constructor( private authService: AuthService, private uiService: UIService ) { }
 
   ngOnInit() {
+    this.loadingSubs = this.uiService.loadingStateChanged.subscribe(isLoading => {
+      this.isLoading = isLoading
+    })
     this.loginForm = new FormGroup({
       email: new FormControl('', { validators: [Validators.required, Validators.email] }),
       password: new FormControl('', { validators: [Validators.required] })
@@ -25,6 +31,10 @@ export class LoginComponent implements OnInit {
       email: this.loginForm.value.email,
       password: this.loginForm.value.password
     })
+  }
+
+  ngOnDestroy() {
+    this.loadingSubs.unsubscribe()
   }
 
 }
