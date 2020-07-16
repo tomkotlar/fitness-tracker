04d8c551bc8fd2df8dd72e69657b22f6604ea690
tomkotlar <t.kotlar@yahoo.com>
2020-07-16 16:18:27 +0100

    Build -Spliting Auth Routing from App.routing

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from "@angular/core";
 import { ReactiveFormsModule } from '@angular/forms';
 
-
 import { SignupComponent } from './signup/signup.component';
 import { LoginComponent } from './login/login.component';
 import { AngularFireAuthModule } from '@angular/fire/auth';
 import { SharedModule } from '../shared/shared.module';
+import { AuthRoutingModule } from './auth.routing.module';
 
 @NgModule({
     declarations: [
@@ -15,7 +15,8 @@ import { SharedModule } from '../shared/shared.module';
     imports: [
         ReactiveFormsModule,
         AngularFireAuthModule,
-        SharedModule
+        SharedModule,
+        AuthRoutingModule,
     ],
     exports: []
 
